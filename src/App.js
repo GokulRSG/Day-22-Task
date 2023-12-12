8f5dfd8150e433c911debe107be144ede89ef67b
GokulRSG <gokulnath2718@gmail.com>
2023-12-12 22:08:05 +0530

    Day-22-Task

--- src/App.js
+++ src/App.js
@@ -1,23 +1,69 @@
 import logo from './logo.svg';
 import './App.css';
+import {Check, X} from 'react-bootstrap-icons'
+import FormText from 'react-bootstrap/FormText'
+import { Button } from 'react-bootstrap';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='card'>
+        <div className='card-head'>
+          <p>FREE</p>
+          <h2>$0/month</h2>
+        </div>
+        <div className='card-body'>
+          <div className='card-p'><Check className='svg' /><p>single user</p></div>
+          <div className='card-p'><Check className='svg'/><p>50GB Storage</p></div>
+          <div className='card-p'><Check className='svg'/><p>Unlimited Public Projects</p></div>
+          <div className='card-p'><Check className='svg'/><p>Community Access</p></div>
+          <div className="card-p card-bg"><X className='svg'/><p>Unlimited Private Projects</p></div>
+          <div className="card-p card-bg"><X className='svg'/><p>Dedicated Phone Support</p></div>
+          <div className="card-p card-bg"><X className='svg'/><p>Free Subdomain</p></div>
+          <div className="card-p card-bg"><X className='svg'/><p>Monthly Status Report</p></div>
+        </div>
+        <div className='btn-contain'>
+          <button>Button</button>
+        </div>
+      </div>
+      <div className='card'>
+        <div className='card-head'>
+          <p>PLUS</p>
+          <h2>$9/month</h2>
+        </div>
+        <div className='card-body'>
+          <div className='card-p'><Check className='svg' /><p>single user</p></div>
+          <div className='card-p'><Check className='svg'/><p>50GB Storage</p></div>
+          <div className='card-p'><Check className='svg'/><p>Unlimited Public Projects</p></div>
+          <div className='card-p'><Check className='svg'/><p>Community Access</p></div>
+          <div className='card-p'><Check className='svg'/><p>Unlimited Private Projects</p></div>
+          <div className='card-p'><Check className='svg'/><p>Dedicated Phone Support</p></div>
+          <div className='card-p'><Check className='svg'/><p>Free Subdomain</p></div>
+          <div className="card-p card-bg"><X className='svg'/><p>Monthly Status Report</p></div>
+        </div>
+        <div className='btn-contain'>
+          <button>Button</button>
+        </div>
+      </div>
+      <div className='card'>
+        <div className='card-head'>
+          <p>PRO</p>
+          <h2>$49/month</h2>
+        </div>
+        <div className='card-body'>
+          <div className='card-p'><Check className='svg' /><p>single user</p></div>
+          <div className='card-p'><Check className='svg'/><p>50GB Storage</p></div>
+          <div className='card-p'><Check className='svg'/><p>Unlimited Public Projects</p></div>
+          <div className='card-p'><Check className='svg'/><p>Community Access</p></div>
+          <div className='card-p'><Check className='svg'/><p>Unlimited Private Projects</p></div>
+          <div className='card-p'><Check className='svg'/><p>Dedicated Phone Support</p></div>
+          <div className='card-p'><Check className='svg'/><p>Free Subdomain</p></div>
+          <div className='card-p'><Check className='svg'/><p>Monthly Status Report</p></div>
+        </div>
+        <div className='btn-contain'>
+          <button>Button</button>
+        </div>
+      </div>
     </div>
   );
 }
